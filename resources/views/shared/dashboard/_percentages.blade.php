1dcd9737977c841a7c8192a8b8e35587cf8d1011
Kuma Stéphane <kumastephane@gmail.com>
2022-06-25 14:13:22 +0000

    Dashboard view updated

--- resources/views/shared/dashboard/_percentages.blade.php
+++ resources/views/shared/dashboard/_percentages.blade.php
@@ -1,5 +1,5 @@
 @if($studentCount > 0)
-    <div class="mt-3 mb-3 d-flex align-items-center">
+    <div class="row mt-3 mb-3 d-flex align-items-center">
         <div class="col-3">
             <span class="ps-2 me-2">Elèves %</span>
             <span class="badge rounded-pill border text-white px-3" style="background-color: #1980c4;">Mâle</span>
@@ -13,12 +13,14 @@
             $femaleStudentPercentage = round((($studentCount - $maleStudentsCount) / $studentCount), 2) * 100;
             $femaleStudentPercentageStyle = "style='background-color: #49a4fe; width: $femaleStudentPercentage%'";
         @endphp
-        <div class="col-9 progress">
-            <div class="progress-bar progress-bar-striped" role="progressbar" {!! $maleStudentPercentageStyle !!} aria-valuenow="{{ $maleStudentPercentage }}" aria-valuemin="0" aria-valuemax="100">
-                {{ $maleStudentPercentage }}%
-            </div>
-            <div class="progress-bar progress-bar-striped" role="progressbar" {!! $femaleStudentPercentageStyle !!} aria-valuenow="{{ $femaleStudentPercentage }}" aria-valuemin="0" aria-valuemax="100">
-                {{ $femaleStudentPercentage }}%
+        <div class="col-9">
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped" role="progressbar" {!! $maleStudentPercentageStyle !!} aria-valuenow="{{ $maleStudentPercentage }}" aria-valuemin="0" aria-valuemax="100">
+                    {{ $maleStudentPercentage }}%
+                </div>
+                <div class="progress-bar progress-bar-striped" role="progressbar" {!! $femaleStudentPercentageStyle !!} aria-valuenow="{{ $femaleStudentPercentage }}" aria-valuemin="0" aria-valuemax="100">
+                    {{ $femaleStudentPercentage }}%
+                </div>
             </div>
         </div>
     </div>
