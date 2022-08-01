7386b6fef45630a9264ec36277256d3fa3218238
Kuma Stéphane <kumastephane@gmail.com>
2022-08-01 19:08:43 +0000

    Footer link fixed

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -2,7 +2,7 @@
 <footer id="page-footer" class="opacity-0">
     <div class="content py-20 font-size-sm clearfix">
         <div class="float-right">
-            Avec <i class="fa fa-heart text-pulse"></i> par <a class="font-w600" href="https://stkservice.tech" target="_blank">STKSERVICES</a>
+            Avec <i class="fa fa-heart text-pulse"></i> par <a class="font-w600" href="https://stkservices.tech" target="_blank">STKSERVICES</a>
         </div>
         <div class="float-left">
             <a class="font-w600" href="https://sms.stkservice.tech" target="_blank">STK SMS 1.0.0</a> &copy; <span class="js-year-copy"></span>
